1cf2c6219d6f3f6515a6bf724b86206111156106
Prajwal V Naik <prajwalv346@gmail.com>
2023-04-11 11:32:35 +0530

    js update_2

--- Github_API/index.js
+++ Github_API/index.js
@@ -15,34 +15,34 @@ function getProfile() {
     Promise.all([fetch(profileUrl), fetch(followersUrl)])
     .then(responses => Promise.all(responses.map(response => response.json())))
     .then(data => {
-        
+        followerslist.innerHTML = '';
         const profileData = data[0];
         const followersData = data[1];
         
         console.log(profileData)
-        if(profileData.message == 'Not Found'){
-          let profile = document.getElementById("profile");
-          let followers = document.getElementById("followers");
-          let head1 = document.createElement("h1");
-          head1.textContent = '404';
-          head1.classList.add('js_heads');
-          let head2 = document.createElement("h1");
-          head2.textContent = '404';
-          head2.classList.add('js_heads');
-          profile.append(head1);
-          followers.append(head2);
+        if (profileData.message != 'Not Found') {
+            profilePic.src = profileData.avatar_url;
+            userid.textContent = profileData.name;
+            bio.textContent = profileData.bio;
+            repo.textContent = `repositories = ${profileData.public_repos}`;
+            
+            followersData.map(follower => {
+              let list = document.createElement('p');
+              list.textContent = follower.login;
+              followerslist.append(list);
+            });
         }
-        else{
-        profilePic.src = profileData.avatar_url;
-        userid.textContent = profileData.name;
-        bio.textContent = profileData.bio;
-        repo.textContent = `repositories = ${profileData.public_repos}`;
-        
-        followersData.map(follower => {
-          let list = document.createElement('p');
-          list.textContent = follower.login;
-          followerslist.append(list);
-        });
+        else {
+            let profile = document.getElementById("profile");
+            let followers = document.getElementById("followers");
+            let head1 = document.createElement("h1");
+            head1.textContent = '404';
+            head1.classList.add('js_heads');
+            let head2 = document.createElement("h1");
+            head2.textContent = '404';
+            head2.classList.add('js_heads');
+            profile.append(head1);
+            followers.append(head2);
         }
       })
     .catch(error => console.log(error));
